f27916f80929cba14d358059dbf2708fbdf12aeb
NMT <nguyenminhtri.20122001@gmail.com>
2022-12-01 10:25:43 +0700

    TÌm phần tử lặp lại - Update

--- 11_30_BT.c
+++ 11_30_BT.c
@@ -24,25 +24,52 @@ void HienThi(int arr[], int length)
         printf("%d\t", arr[i]);
     }
 }
+void Tang_Dan(int *t, int length)
+{
+    for(int i = 0; i<length; i++)
+    {
+        for(int j = 0; j <length; j++)
+        {
+                            // printf("%d\t\n",*(t + i));   
+            if(*(t + i) <  *(t+j))
+            {
+                int temp = *(t+j);
+                *(t+j) = *(t + i);
+                *(t + i) = temp;
+            }
+
+        }
+    }
+            printf("\nTang Dan: ");       
+    for(int i = 0; i<length; i++)
+    {     
+            printf("%d\t",*(t + i));   
+    }
+    
+}
   void LapLai(int arr[], int length)
   {
-    int max = 1;
-    for (int i = 0; i < length; i++)
-    {
-      int dem = 0;
-      for (int j = 0; j < length; j++)
-      {
-      if ((arr[i] == arr[j])) dem++;
-      }
-      if (max < dem)
-      {
-        // int SPT = 0;
-        printf("\nPhan tu lap la: %d\nVoi so lan xuat hien la: %d\n", arr[i], dem);   
-        // printf("So phan tu %d", SPT);
-        // SPT++;
-      }
-    }     
-  }
+    Tang_Dan(arr, length);
+  
+	int temp = arr[0];
+	int dem = 0;
+	for (int i = 0; i <length; i++)
+	{
+		if (arr[i]== temp)
+		{
+			dem++;
+		}
+		else
+		{
+			printf("\nGia tri %d xuat hien %d lan", temp, dem);
+			temp = arr[i];
+			dem = 1;
+		}
+	}
+      //Phần tử cuối cùng gán nhưng chưa in ra
+			printf("\nGia tri %d xuat hien %d lan", temp, dem);
+  }     
+  
 int main(int argc, char const *argv[])
 {
     int *arr;
